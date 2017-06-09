8de2f593e9973d239b318ba1e12013e9ec5dbbc5
zhang1993 <zhangmingyang1@gmail.com>
2017-06-09 22:34:02 +0800

    leetCode第87题题解

---
+++ Scramble String/src/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Shinelon on 2017/6/9.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print(new Solution().isScramble("abc","bca"));
+    }
+    public boolean isScramble(String s1, String s2) {
+        if(s1.equals(s2))
+            return true;
+        if(s1.length() != s2.length())
+            return false;
+        int[] letters = new int[26];
+        for (int i=0; i<s1.length(); i++) {
+            letters[s1.charAt(i)-'a']++;
+            letters[s2.charAt(i)-'a']--;
+        }
+        for (int i=0; i<26; i++) if (letters[i]!=0) return false;
+        for(int i = 1; i < s1.length(); i++) {
+            if(isScramble(s1.substring(0, i), s2.substring(0, i)) &&  isScramble(s1.substring(i), s2.substring(i)))
+                return true;
+            if(isScramble(s1.substring(0, i), s2.substring(s1.length() - i)) && isScramble(s2.substring(0,s1.length() -  i), s1.substring(i)))
+                return true;
+        }
+        return false;
+    }
+}
